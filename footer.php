1649979d1c6477b319fe57395e8623cee8ff0c80
kishor-cosmodea <kishor@cosmodea.com>
2016-02-24 18:42:41 +0530

    #4 Footer structure and style created

--- footer.php
+++ footer.php
@@ -1,9 +1,38 @@
 	
-	<footer>
+<!-- Footer starts here -->
+<footer>
+	<div class="wrapper">
+		<div class="foot-content">
+			<ul>
+				<li><a href="about-us.php" title="About Us">About Us</a></li>
+				<li><a href="terms.php" title="Terms">Terms</a></li>
+				<li><a href="privacy-policy.php" title="Privacy Policy">Privacy Policy</a></li>
+			</ul>
+			<ul>
+				<li><a href="mailto:support@policynote.com"><span class="email"></span>support@policynote.com</a></li>
+				<li><a href="tel:022-22222222"><span class="call"></span>022-22222222</a></li>
+			</ul>
+			<ul>
+				<li>Follow Us</li>
+				<li>
+					<a href="https://www.facebook.com/" target="_blank" class="fb" title="Facebook">fb</a>
+					<a href="https://twitter.com/" target="_blank" class="tw" title="Twitter">tw</a>
+				</li>
+			</ul>
+			<p>
+				This Site Is Being Operated By Policynote Insurance, 
+				an IRDAI Certified Broker Under License No. IRDAI/AAAAAA/22, Valid Till: 02/02/2022
+			</p>
+			<p>
+				Insurance Is The Subject Matter Of The Solicitation.
+			</p>
+			<p class="pnote">&copy; 2016 <a href="index.php" title="Policynote">Policynote</a>. All Rights Reserved.</p>
+		</div>
+	</div>
+</footer>
+<!-- Footer ends here -->
 
-	</footer>
-
-    <script src="assets/js/script.js"></script>
-    <!-- <script src="//code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script> -->
- </body>
- </html>
+<script src="assets/js/script.js"></script>
+<!-- <script Src="//code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script> -->
+</body>
+</html>
