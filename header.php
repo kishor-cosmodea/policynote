fffbd004e5f63e3e7b880427c6b371d99db1b40c
kishor-cosmodea <kishor@cosmodea.com>
2016-02-24 16:32:26 +0530

    #3 Header structure and style created

--- header.php
+++ header.php
@@ -3,11 +3,11 @@
 <!-- If multi-language site, reconsider usage of html lang declaration here. -->
 <html lang="en">
 
-  <head>
+<head>
 
     <meta charset="utf-8">
 
-    <title>Title</title>
+    <title>Policynote</title>
 
     <!-- 120 word description for SEO purposes goes here. Note: Usage of lang tag. -->
     <meta name="description" lang="en" content="">
@@ -19,9 +19,9 @@
     <!-- This meta tag is used for mobile device to display the page without any zooming,
          so how much the device is able to fit on the screen is what's shown initially.
          Remove comments from this tag, when you want to apply media queries to the website. -->
-    <!--<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">-->
+         <!--<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">-->
 
-    <!-- To adhear no-cache for Chrome -->
+         <!-- To adhear no-cache for Chrome -->
     <!-- <meta http-equiv="cache-control" content="no-store, no-cache, must-revalidate" />
     <meta http-equiv="Pragma" content="no-store, no-cache" />
     <meta http-equiv="Expires" content="0" />
@@ -32,6 +32,9 @@
 
     <!-- Default style-sheet is for 'media' type screen (color computer display).  -->
     <link rel="stylesheet" media="screen" href="assets/css/style.css" >
+
+    <!-- Open sans google font -->
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800italic,800' rel='stylesheet' type='text/css'>
 
     <!--jQuery -->
     <script src="assets/vendor/jquery-2.2.0.min.js"></script>
@@ -44,6 +47,22 @@
     <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
     <![endif]-->
 
-  </head>
-  <body>
-    
+</head>
+<body>
+    <header>
+        <div class="wrapper cf">
+            <div class="head-content">
+                <h1>
+                    <a href="index.php" title="Policynote">POLICYNOTE</a>
+                </h1>
+                <nav>
+                    <ul>
+                        <li><a href="index.php" title="Home" class="active">HOME</a></li>
+                        <li><a href="car.php" title="Car">CAR</a></li>
+                        <li><a href="health.php" title="Health">HEALTH</a></li>
+                        <li><a href="about-us.php" title="About Us">ABOUT US</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </header>
