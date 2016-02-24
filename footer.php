cfc511d6135d37d7592b52bda6e19272337381a0
kishor-cosmodea <kishor@cosmodea.com>
2016-02-24 14:23:45 +0530

    #2 Dev framework updated

---
+++ footer.php
@@ -0,0 +1,9 @@
+	
+	<footer>
+
+	</footer>
+
+    <script src="assets/js/script.js"></script>
+    <!-- <script src="//code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script> -->
+ </body>
+ </html>
