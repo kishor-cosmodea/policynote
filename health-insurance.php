de4cd8c7580111381d6dcf07ba3e2eb78662523e
kishor-cosmodea <kishor@cosmodea.com>
2016-02-26 11:34:21 +0530

    #7 Health insurance page added

---
+++ health-insurance.php
@@ -0,0 +1,28 @@
+<?php
+	$description="";
+	$pagetitle="Health Insurance |";
+	include("header.php");
+?>
+
+<!-- Content starts here-->
+<div class="container">
+	<div class="health-content">
+		<div class="health-text">
+			<h2>Get Insurance for your Health</h2>
+		</div>
+		<div class="health-quote">
+			<div class="health-get">
+				<ul>
+					<li><button>Pin Code</button></li>
+				</ul>
+				<button class="btn-quote">Get Quotes</button>
+			</div>
+		</div>
+	</div>
+	<div class="middle-content">
+
+	</div>
+</div>
+<!-- Content ends here -->
+
+<?php include("footer.php"); ?>
