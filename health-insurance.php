779c5143cd4f3b0f7dee3820a7844eb4420ba294
kishor-cosmodea <kishor@cosmodea.com>
2016-02-26 19:02:16 +0530

    #9 Health insurance page updated

--- health-insurance.php
+++ health-insurance.php
@@ -9,18 +9,74 @@
 	<div class="health-content">
 		<div class="health-text">
 			<h2>Get Insurance for your Health</h2>
+			<h3>New Health Policy</h3>
 		</div>
 		<div class="health-quote">
 			<div class="health-get">
 				<ul>
-					<li><button>Pin Code</button></li>
+					<li><span>Pin Code </span><input class="health-pin" type="text" name="health-pin"></li>
+					<li><span>Insurance For :</span></li>
+					<li>
+						<button>Self</button>
+						<button>Spouse</button>
+						<button>Father</button>
+						<button>Mother</button>
+						<button>Son</button>
+						<button>Daughter</button>
+					</li>
+					<li><span>Insured member details are :</span></li>
+					<li>
+						<span>Self :</span><input class="health-age" type="text" name="health-pin"><span>Years</span>
+						<button>Male</button>
+						<button>Female</button>
+					</li>
 				</ul>
 				<button class="btn-quote">Get Quotes</button>
 			</div>
 		</div>
 	</div>
 	<div class="middle-content">
-
+		<div class="list-insurance">
+			<div class="ins-head">
+				<h2>LIST OF COMPANIES WE ARE AFFILIATED WITH</h2>
+			</div>
+			<div class="ins-img">
+				<img src="assets/images/insurance-comp.jpg" alt="Ins">
+			</div>
+			<div class="ins-head">
+				<h2>INSURANCE THOUGHTS</h2>
+			</div>
+			<div class="ins-thoug">
+				<p>
+					<img src="assets/images/comma.png" alt="comma">
+					Policynote has managed to do what no one else has. Buying insurance has never been this easy. Policynote has managed to do what no one else has. Buying insurance has never been this easy.
+				</p>
+				<p>
+					<img src="assets/images/comma.png" alt="comma">
+					Policynote has managed to do what no one else has. Buying insurance has never been this easy. Policynote has managed to do what no one else has. Buying insurance has never been this easy.
+				</p>
+			</div>
+			<div class="easy-save">
+				<div class="easy-wrap">
+					<p>
+						<img src="assets/images/easy.png" alt="easy">
+						<span>EASY</span>
+					</p>
+					<p>
+						<img src="assets/images/simple.png" alt="simple">
+						<span>SIMPLE</span>
+					</p>
+					<p>
+						<img src="assets/images/save.png" alt="save">
+						<span>SAVE</span>
+					</p>
+					<p>
+						<img src="assets/images/quick.png" alt="quick">
+						<span>QUICK</span>
+					</p>
+				</div>
+			</div>
+		</div>
 	</div>
 </div>
 <!-- Content ends here -->
