f97bfc2f14b8d98165d61f8f8983cd2ab5e7f86d
kishor-cosmodea <kishor@cosmodea.com>
2016-02-29 18:40:15 +0530

    #11 Added health compare page

---
+++ health-compare.php
@@ -0,0 +1,69 @@
+<?php
+$description="";
+$pagetitle="Health Compare |";
+include("header.php");
+?>
+
+<!-- Content starts here-->
+<div class="container">
+	<div class="wrapper">
+		<div class="car-profile-content">
+			<div class="car-profile-text">
+				<h2>Buy Health Insurance</h2>
+			</div>
+			<div class="car-refine">
+				<div class="car-re-data">
+					<h3>Refine Search</h3>
+					<span>Pin Code</span>
+					<button>Pin Code</button>
+					<span>Insured for</span>
+					<button>Self</button>
+					<button>Spouse</button>
+					<button>Father</button>
+					<button>Mother</button>
+					<button>Son</button>
+					<button>Daughter</button>
+					<span>Insured member details are:</span>
+				</div>
+				<div class="car-re-add">
+					<span>Add Ons</span>
+					<button>Zero Depriciation</button>
+					<button>Engine Protect</button>
+					<button>NCB Protect</button>
+				</div>
+				<div class="car-re-dis">
+					<button>Discounts</button>
+				</div>
+				<button class="ref-submit">Submit</button>
+			</div>
+			<div class="car-result-data">
+				<div class="car-plan">
+					<span>Policy Plans</span>
+					<div>
+						<span class="car-sort">Sort By:</span>
+						<span>Price</span>
+					</div>
+				</div>
+
+				<div class="car-policy-plan">
+					<p>
+						<img src="assets/images/apollo.png" alt="">
+						<span class="car-idv">Insured Declared Value (IDV)</span>
+						<span>Rs. 5,00,850</span>
+					</p>
+					<p class="car-premium">
+						<span class="car-amt">Rs. 10,123</span>
+						<span class="car-det">Premium Details</span>
+					</p>
+					<p>
+						<button class="car-buy">Buy</button>
+					</p>
+				</div>
+
+			</div>
+		</div>
+	</div>
+</div>
+<!-- Content ends here -->
+
+<?php include("footer.php"); ?>
