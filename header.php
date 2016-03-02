0762d18b3de15d8fb76a506b3f46803b21d11dce
kishor-cosmodea <kishor@cosmodea.com>
2016-03-02 19:40:09 +0530

    #14 Home page updated

--- header.php
+++ header.php
@@ -54,14 +54,14 @@
         <div class="wrapper cf">
             <div class="head-content">
                 <h1>
-                    <a href="index.php" title="Policynote">POLICYNOTE</a>
+                    <a href="index.php" title="Policynote">Policynote</a>
                 </h1>
                 <nav>
                     <ul>
-                        <li><a id="nav-home" href="index.php" title="Home">HOME</a></li>
-                        <li><a id="nav-car" href="car-insurance.php" title="Car">CAR</a></li>
-                        <li><a id="nav-health" href="health-insurance.php" title="Health">HEALTH</a></li>
-                        <li><a id="nav-about" href="about-us.php" title="About Us">ABOUT US</a></li>
+                        <li><a id="nav-home" href="index.php" title="Home">Home</a></li>
+                        <li><a id="nav-car" href="car-insurance.php" title="Car">Car</a></li>
+                        <li><a id="nav-health" href="health-insurance.php" title="Health">Health</a></li>
+                        <li><a id="nav-about" href="about-us.php" title="About Us">About Us</a></li>
                     </ul>
                 </nav>
             </div>
--- index.php
+++ index.php
@@ -9,13 +9,56 @@ include("header.php");
 	<div class="main-content">
 		<div class="main-text">
 			<h2>Adding Insurance to Life</h2>
-			<h3>Get Quotes</h3>
-			<a href="car-insurance.php" title="Car"><span class="btn-car"></span><span class="t-car">CAR</span></a>
-			<a href="health-insurance.php" title="Health"><span class="btn-health"></span><span class="t-health">HEALTH</span></a>
+			<h3>Get Quotes :</h3>
+			<a href="car-insurance.php" title="Car"><span class="btn-car"></span><span class="t-car">Car</span></a>
+			<a href="health-insurance.php" title="Health"><span class="btn-health"></span><span class="t-health">Health</span></a>
 		</div>
 	</div>
 	<div class="middle-content">
-		<div class="wrapper">
+		<div class="list-insurance">
+			<div class="ins-head">
+				<h2>LIST OF COMPANIES WE ARE AFFILIATED WITH</h2>
+			</div>
+			<div class="ins-img">
+				<img src="assets/images/insurance-comp.jpg" alt="Ins">
+			</div>
+
+			<div class="car-spl-data cf">
+				<div class="spl-one-cont">
+					<p><img src="assets/images/dummy-200x200-Rope.jpg" alt="car"></p>
+					<p class="car-tech">
+						<span>Lorem Ipsum is simply dummy text of the printing</span>
+						Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+						Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+						when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+						It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+					</p>
+				</div>
+
+				<div class="spl-one-cont">
+					<p class="car-left">
+						<span>Lorem Ipsum is simply dummy text of the printing</span>
+						Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+						Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+						when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+						It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+					</p>
+					<p><img src="assets/images/dummy-200x200-Rope.jpg" alt="car"></p>
+				</div>
+
+				<div class="spl-one-cont">
+					<p><img src="assets/images/dummy-200x200-Rope.jpg" alt="car"></p>
+					<p class="car-tech">
+						<span>Lorem Ipsum is simply dummy text of the printing</span>
+						Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+						Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+						when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+						It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+					</p>
+				</div>
+
+			</div>
+
 			<div class="sketch-cont">
 				<div>
 					<img src="assets/images/sketch-car.png" alt="car">
@@ -28,36 +71,35 @@ include("header.php");
 					<p>Find the best match health insurance</p>
 				</div>
 			</div>
-		</div>
-		<div class="list-insurance">
-			<div class="ins-head">
-				<h2>LIST OF COMPANIES WE ARE AFFILIATED WITH</h2>
+
+			<div class="car-bg-lay">
+				<div class="ins-head">
+					<h2>INSURANCE THOUGHTS</h2>
+				</div>
+				<div class="ins-thoug">
+					<p>
+						<img src="assets/images/comma.png" alt="comma">
+						Policynote has managed to do what no one else has. Buying insurance has never been this easy. Policynote has managed to do what no one else has. Buying insurance has never been this easy.
+					</p>
+					<p>
+						<img src="assets/images/comma.png" alt="comma">
+						Policynote has managed to do what no one else has. Buying insurance has never been this easy. Policynote has managed to do what no one else has. Buying insurance has never been this easy.
+					</p>
+				</div>
 			</div>
-			<div class="ins-img">
-				<img src="assets/images/insurance-comp.jpg" alt="Ins">
-			</div>
-			<div class="ins-head">
-				<h2>INSURANCE THOUGHTS</h2>
-			</div>
-			<div class="ins-thoug">
-				<p>
-					<img src="assets/images/comma.png" alt="comma">
-					Policynote has managed to do what no one else has. Buying insurance has never been this easy. Policynote has managed to do what no one else has. Buying insurance has never been this easy.
-				</p>
-				<p>
-					<img src="assets/images/comma.png" alt="comma">
-					Policynote has managed to do what no one else has. Buying insurance has never been this easy. Policynote has managed to do what no one else has. Buying insurance has never been this easy.
-				</p>
+			<div class="main-testi">
+				<h2>“Policynote has done a great job. I renewed my car policy in 10 minutes. It’s quite incredible.”</h2>
+				<span>- Jack Son</span>
 			</div>
 			<div class="easy-save">
 				<div class="easy-wrap">
 					<p>
 						<img src="assets/images/easy.png" alt="easy">
-						<span>EASY</span>
+						<span>SECURE</span>
 					</p>
 					<p>
 						<img src="assets/images/simple.png" alt="simple">
-						<span>SIMPLE</span>
+						<span>EASY</span>
 					</p>
 					<p>
 						<img src="assets/images/save.png" alt="save">
