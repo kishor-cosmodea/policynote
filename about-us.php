9c28c9ac79fc8477b43db3333582a35a957184af
kishor-cosmodea <kishor@cosmodea.com>
2016-03-05 18:02:19 +0530

    #21 Added about us page

---
+++ about-us.php
@@ -0,0 +1,73 @@
+<?php
+$description="";
+$pagetitle="About Us |";
+include("header.php");
+?>
+
+<!-- Content starts here-->
+<div class="container">
+	<div class="about-content">
+		<div class="about-text">
+			<div class="wrapper">
+				<h2>Insurance Matters</h2>
+				<h3>Lorem Ipsum is simply dummy text of!</h3>
+			</div>
+		</div>
+	</div>
+	<div class="about-family">
+		<div class="wrap-family">
+			<div>
+				<img src="assets/images/about-family.png" alt="family">
+			</div>
+			<div>
+				<h2>For Family</h2>
+				<h3>Lorem Ipsum is simply dummy text of!</h3>
+			</div>
+		</div>
+	</div>
+	<div class="about-policy">
+		<div class="wrap-policy">
+			<div>
+				<h2>Get Policy from Policynote</h2>
+				<h3>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</h3>
+			</div>
+		</div>
+	</div>
+
+	<div class="about-buy">
+		<div class="wrap-buy">
+			<div>
+				<h2><span>Policynote</span> Get policy today!</h2>
+			</div>
+			<div>
+				<span class="about-want">I want to buy</span>
+				<button>Select Insurance Type</button>
+				<button id="about-get">Get Started</button>
+			</div>
+		</div>
+	</div>
+
+	<div class="about-cont">
+		<div class="wrap-cont">
+			<div>
+				<h2>Location</h2>
+			</div>
+			<div class="about-street">
+				<p>Policynote</p>
+				<span>02, New Street,</span>
+				<span>Star Villa,</span>
+				<span>Mumbai-400001</span>
+				<p class="about-us">Write to us</p>
+				<span>support@policynote.com</span>
+				<span>022-22222222</span>
+			</div>
+			<div class="about-add">
+				<img src="assets/images/about-map.png" alt="map">
+			</div>
+		</div>
+	</div>
+
+</div>
+<!-- Content ends here -->
+
+<?php include("footer.php"); ?>
