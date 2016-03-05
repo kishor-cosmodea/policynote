cef678b77c73df7b8983e28ffb95fe35b1084d71
kishor-cosmodea <kishor@cosmodea.com>
2016-03-05 18:34:50 +0530

    #22 Added terms and privacy policy page

---
+++ privacy-policy.php
@@ -0,0 +1,52 @@
+<?php
+$description="";
+$pagetitle="Policy Policy |";
+include("header.php");
+?>
+
+<!-- Content starts here-->
+<div class="container">
+
+	<div class="terms-content">
+		<div class="wrapper">
+			<h2>Policynote Privacy Policy</h2>
+
+			<div class="terms-use">
+				<h3>Personal identification information</h3>
+				<p>
+					Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+					Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+				</p>
+			</div>
+
+			<div class="terms-use">
+				<h3>Web browser cookies</h3>
+				<p>
+					Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+					Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+				</p>
+			</div>
+
+			<div class="terms-use">
+				<h3>Changes to this privacy policy</h3>
+				<p>
+					Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+					Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+				</p>
+			</div>
+
+			<div class="terms-use">
+				<h3>Your acceptance of these terms</h3>
+				<p>
+					Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+					Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+				</p>
+			</div>
+
+		</div>
+	</div>
+
+</div>
+<!-- Content ends here -->
+
+<?php include("footer.php"); ?>
---
+++ terms.php
@@ -0,0 +1,52 @@
+<?php
+$description="";
+$pagetitle="Terms |";
+include("header.php");
+?>
+
+<!-- Content starts here-->
+<div class="container">
+
+	<div class="terms-content">
+		<div class="wrapper">
+			<h2>Policynote Terms of Use</h2>
+
+			<div class="terms-use">
+				<h3>Terms of Use Agreement</h3>
+				<p>
+					Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+					Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+				</p>
+			</div>
+
+			<div class="terms-use">
+				<h3>Using Policynote</h3>
+				<p>
+					Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+					Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+				</p>
+			</div>
+
+			<div class="terms-use">
+				<h3>Governing Law</h3>
+				<p>
+					Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+					Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+				</p>
+			</div>
+
+			<div class="terms-use">
+				<h3>Guarantee</h3>
+				<p>
+					Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+					Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+				</p>
+			</div>
+
+		</div>
+	</div>
+
+</div>
+<!-- Content ends here -->
+
+<?php include("footer.php"); ?>
